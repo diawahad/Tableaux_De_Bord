f338877603dd538ff47a749c209e27b68a1282fe
Serigne <diawahad@gmail.com>
2018-02-22 16:08:07 +0100

    new docs

---
+++ Code_Source/wade_scrapy/wade_scrapy/spiders/easy.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 10:46:43 2018
+
+@author: samba
+"""
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "projet"
+    start_urls = [
+        'https://www.journaldunet.com/ebusiness/commerce/list/2007-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2006-11-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2008-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2009-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2010-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2011-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2012-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2013-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2014-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2015-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2016-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2017-12-1/',
+        'https://www.journaldunet.com/ebusiness/commerce/list/2018-2-1/'
+        
+    ]
+
+    def parse(self, response):
+        for quote in response.selector.xpath('//html'):
+            yield {
+                'lien': quote.select('//li/h4/a/@href').extract(),
+                'tittle': quote.select('//li/div/div[@class="grid_left"]/a/@title').extract(),
+                #'data' : quote.select('//div[@class="app_box_3 abs"]/div[@class="content"]/text()').extract(),
+                #'key_word' : quote.select('//ul[@class="app_tags_list"]/li/a/text()').extract(),
+
+            }
+        next_page = response.selector.xpath('//section[@class="ccmcss_paginator ccmcss_paginator--date"]/div[1]/ul/li/a[@class=""]/@href').extract()
+        print(next_page)
+        for i in range (len(next_page)):
+            yield scrapy.Request(next_page[i])
+    
+
+"""
+scrapy crawl projet -o lien.json
+"""
+
+    
+    
+    
+    
+    
---
+++ Code_Source/wade_scrapy/wade_scrapy/spiders/my_crawler.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 17:51:15 2018
+
+@author: sceli
+"""
+#scrapy crawl mycrawler
+import scrapy
+import json
+class MySpider(scrapy.Spider):
+    name = "mycrawler"
+    allowed_domains = ["journaldunet.com"]
+    with open('C:/Users/samba/Desktop/info sid/M2/projet_tab_bord/wade_scrapy/wade_scrapy/spiders/lien2759.json', 'r') as file:
+        data = json.load(file)
+    start_urls = data['lien']
+    
+    def parse(self, response):
+        for quote in response.selector.xpath('//html'):
+            yield {
+                "content" : quote.select("//div[@class='entry']/p/text()|//div[@class='entry']/p/a/text()").extract(),
+                "tags" : quote.select("//section[@class='app_list_2']/aside[@class='app_tags_list see-also']/ul/li/a/text()").extract(),
+                "date" : quote.select("//div[@class='entry']/aside[@class='app_author_box abs']/div/div/div/time/text()").extract(),
+                "author" : quote.select("//div[@class='entry']/aside[@class='app_author_box abs']/div/div/div/dl/dt/a[@rel='author']/text()").extract(),
+                "intro" : quote.select("//p[@class='app_entry_lead']/text()").extract(),
+                "title" : quote.select("//title/text()").extract(),
+                "titletable" : quote.select("//table/caption/text()").extract(),
+                "colnames" :quote.select("//table/thead/tr/th/text()").extract(),
+                "record" : quote.select("//table/tbody/tr/th/text()|//table/tbody/tr/td/text()").extract(),
+                
+            }
+    
